2da5361a542a81004956ab71e7738167b7a0bd01
ddorm106 <centfire6@gmail.com>
2026-01-02 16:34:31 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Minimal service worker for PWA "installable" feel
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('fetch', (e) => {
-    // Optional: cache strategy (can be empty for now)
-});
